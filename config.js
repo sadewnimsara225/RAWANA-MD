16c9bed824fedc200bff9c7c027e3e6d0b1cbfca
Sadew Nimsara <126339418+sadewnimsara225@users.noreply.github.com>
2025-08-01 21:56:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "ENTER YOUR SESSION ID":process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined? "mongodb://mongo:QFOCRrGPnLOgHwoyRCUfEhqzPcUbXLLz@shinkansen.proxy.rlwy.net:26579":process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "mongodb://mongo:QFOCRrGPnLOgHwoyRCUfEhqzPcUbXLLz@shinkansen.proxy.rlwy.net:26579",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
